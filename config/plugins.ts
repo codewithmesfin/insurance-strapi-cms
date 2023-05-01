5ec4036edcf88f0cce77ed565f8af46bf64d5012
System Administrator <sciemesfin55@gmail.com>
2023-05-01 19:26:28 +0300

    password reset

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-
+//gmailapi CLINET_ID:105390029512-8jo979d2mtcl55hdi63hmepsaj250q99.apps.googleusercontent.com
 export default {
     //
     graphql: {
@@ -13,4 +13,19 @@ export default {
         },
       },
     },
+
+    email: {
+      config: {
+        provider: 'sendgrid',
+        providerOptions: {
+          apiKey: "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",
+        },
+        settings: {
+          defaultFrom: 'dev@techethio.com',
+          defaultReplyTo: 'dev@techethio.com',
+          testAddress: 'dev@techethio.com',
+        },
+      },
+    },
+
   };
